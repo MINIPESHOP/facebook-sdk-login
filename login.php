bec5362eeb343b9abc2cff1d9d81fb497e85b58f
- G2P - <guyza01.2547@gmail.com>
2018-01-01 23:05:21 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,58 +1,53 @@
 <!DOCTYPE html>
 <html>
 <style>
-form {
-    border: 3px solid #f1f1f1;
+/* Shared */
+.loginBtn {
+  box-sizing: border-box;
+  position: relative;
+  /* width: 13em;  - apply for fixed size */
+  margin: 0.2em;
+  padding: 0 15px 0 46px;
+  border: none;
+  text-align: left;
+  line-height: 34px;
+  white-space: nowrap;
+  border-radius: 0.2em;
+  font-size: 16px;
+  color: #FFF;
+}
+.loginBtn:before {
+  content: "";
+  box-sizing: border-box;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 34px;
+  height: 100%;
+}
+.loginBtn:focus {
+  outline: none;
+}
+.loginBtn:active {
+  box-shadow: inset 0 0 0 32px rgba(0,0,0,0.1);
 }
 
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-}
 
-button:hover {
-    opacity: 0.8;
+/* Facebook */
+.loginBtn--facebook {
+  background-color: #4C69BA;
+  background-image: linear-gradient(#4C69BA, #3B55A0);
+  /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
+  text-shadow: 0 -1px 0 #354C8C;
 }
-
-.cancelbtn {
-    width: auto;
-    padding: 10px 18px;
-    background-color: #f44336;
+.loginBtn--facebook:before {
+  border-right: #364e92 1px solid;
+  background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
 }
-
-.imgcontainer {
-    text-align: center;
-    margin: 24px 0 12px 0;
-}
-
-img.avatar {
-    width: 40%;
-    border-radius: 50%;
-}
-
-.container {
-    padding: 16px;
-}
-
-span.psw {
-    float: right;
-    padding-top: 16px;
-}
-
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-    span.psw {
-       display: block;
-       float: none;
-    }
-    .cancelbtn {
-       width: 100%;
-    }
+.loginBtn--facebook:hover,
+.loginBtn--facebook:focus {
+  background-color: #5B7BD5;
+  background-image: linear-gradient(#5B7BD5, #4864B1);
 }
 </style>
 <body>
@@ -62,27 +57,23 @@ span.psw {
 session_start();
 ini_set('display_errors', 1);
 require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
-
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
 use Facebook\GraphUser;
 use Facebook\FacebookRedirectLoginHelper;
-
-
 $fb = new Facebook\Facebook([
   'app_id' => 'App ID ของท่าน',
   'app_secret' => 'Secret ID ของท่าน',
   'default_graph_version' => 'v2.5',
 ]);
-
 $helper = $fb->getRedirectLoginHelper();
-
 $permissions = ['email', 'user_likes']; // optional
-
 $loginUrl = $helper->getLoginUrl('http://45.76.146.70/login-callback.php', $permissions); //ที่อยู่ไฟล์ login-callback.php
-echo "<form action="">
-  <button type="submit">
-<a href="' . $loginUrl . '">Login !</a>
+echo '
+<button class="loginBtn loginBtn--facebook"
+<a href="'.$loginUrl.'">Login !</a>
 </button>
-</form>";
+';
 ?>
+</body>
+</html>
